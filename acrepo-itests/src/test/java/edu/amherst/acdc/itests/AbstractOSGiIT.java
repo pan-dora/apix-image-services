c0ca558a2ed77a3791d30574c55c6c32f5f19646
Aaron Coburn <acoburn@amherst.edu>
2016-06-02 17:07:10 -0400

    Add more complete pax-exam testing

---
+++ acrepo-itests/src/test/java/edu/amherst/acdc/itests/AbstractOSGiIT.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2016 Amherst College
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.amherst.acdc.itests;
+
+import static org.apache.http.HttpStatus.SC_CREATED;
+import static org.apache.http.impl.client.HttpClients.createDefault;
+import static org.junit.Assert.assertEquals;
+import static org.osgi.framework.Constants.OBJECTCLASS;
+import static org.osgi.framework.FrameworkUtil.createFilter;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.apache.karaf.features.FeaturesService;
+import org.ops4j.pax.exam.Option;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.util.tracker.ServiceTracker;
+import org.slf4j.Logger;
+
+/**
+ * @author Aaron Coburn
+ * @since May 11, 2016
+ */
+public abstract class AbstractOSGiIT {
+
+    private static Logger LOGGER = getLogger(AbstractOSGiIT.class);
+
+    @Inject
+    protected FeaturesService featuresService;
+
+    @Inject
+    protected BundleContext bundleContext;
+
+    public abstract Option[] config();
+
+    protected String post(final String url) {
+        final CloseableHttpClient httpclient = createDefault();
+        try {
+            final HttpPost httppost = new HttpPost(url);
+            final HttpResponse response = httpclient.execute(httppost);
+            assertEquals(SC_CREATED, response.getStatusLine().getStatusCode());
+            return EntityUtils.toString(response.getEntity(), "UTF-8");
+        } catch (IOException ex) {
+            LOGGER.debug("Unable to extract HttpEntity response into an InputStream: ", ex);
+            return "";
+        }
+    }
+
+    protected <T> T getOsgiService(final Class<T> type, final String filter, final long timeout) {
+        try {
+            final ServiceTracker tracker = new ServiceTracker(bundleContext,
+                    createFilter("(&(" + OBJECTCLASS + "=" + type.getName() + ")" + filter + ")"), null);
+            tracker.open(true);
+            final Object svc = type.cast(tracker.waitForService(timeout));
+            if (svc == null) {
+                throw new RuntimeException("Gave up waiting for service " + filter);
+            }
+            return type.cast(svc);
+        } catch (InvalidSyntaxException e) {
+            throw new IllegalArgumentException("Invalid filter", e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
---
+++ acrepo-itests/src/test/java/edu/amherst/acdc/itests/AcrepoServicesIT.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2016 Amherst College
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.amherst.acdc.itests;
+
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureConsole;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.ConfigurationManager;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.slf4j.Logger;
+
+/**
+ * @author Aaron Coburn
+ * @since May 2, 2016
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class AcrepoServicesIT extends AbstractOSGiIT {
+
+    private static Logger LOGGER = getLogger(AcrepoServicesIT.class);
+
+    @Configuration
+    public Option[] config() {
+        final ConfigurationManager cm = new ConfigurationManager();
+        final String fcrepoPort = cm.getProperty("fcrepo.dynamic.test.port");
+        final String jmsPort = cm.getProperty("fcrepo.dynamic.jms.port");
+        final String reindexingPort = cm.getProperty("fcrepo.dynamic.reindexing.port");
+        final String rmiRegistryPort = cm.getProperty("karaf.rmiRegistry.port");
+        final String rmiServerPort = cm.getProperty("karaf.rmiServer.port");
+        final String fcrepoBaseUrl = "localhost:" + fcrepoPort + "/fcrepo/rest";
+        final String sshPort = cm.getProperty("karaf.ssh.port");
+        final String emptyTopic = "broker:topic:test";
+        final String brokerUrl = "tcp://localhost:" + jmsPort;
+        final String triplestoreBaseUrl = "localhost:" + fcrepoPort + "/fuseki/test/update";
+
+        return new Option[] {
+            karafDistributionConfiguration()
+                .frameworkUrl(maven().groupId("org.apache.karaf").artifactId("apache-karaf")
+                        .versionAsInProject().type("zip"))
+                .unpackDirectory(new File("target", "exam"))
+                .useDeployFolder(false),
+            logLevel(LogLevel.WARN),
+            keepRuntimeFolder(),
+            configureConsole().ignoreLocalConsole(),
+            features(maven().groupId("org.apache.karaf.features").artifactId("standard")
+                        .versionAsInProject().classifier("features").type("xml"), "scr"),
+            features(maven().groupId("org.apache.camel.karaf").artifactId("apache-camel")
+                        .type("xml").classifier("features").versionAsInProject(), "camel-blueprint"),
+            features(maven().groupId("org.apache.activemq").artifactId("activemq-karaf")
+                        .type("xml").classifier("features").versionAsInProject(), "activemq-camel"),
+            features(maven().groupId("edu.amherst.acdc").artifactId("acrepo-karaf")
+                        .type("xml").classifier("features").versionAsInProject(), "acrepo-idiomatic",
+                    "acrepo-idiomatic-pgsql", "acrepo-mint-service", "acrepo-xml-metadata",
+                    "acrepo-jsonld-service", "acrepo-jsonld-cache", "acrepo-template-mustache"),
+
+            systemProperty("karaf.reindexing.port").value(reindexingPort),
+            systemProperty("fcrepo.port").value(fcrepoPort),
+
+            editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiRegistryPort", rmiRegistryPort),
+            editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiServerPort", rmiServerPort),
+            editConfigurationFilePut("etc/org.apache.karaf.shell.cfg", "sshPort", sshPort),
+            editConfigurationFilePut("etc/org.fcrepo.camel.indexing.triplestore.cfg", "fcrepo.baseUrl", fcrepoBaseUrl),
+            editConfigurationFilePut("etc/org.fcrepo.camel.indexing.triplestore.cfg", "triplestore.baseUrl",
+                    triplestoreBaseUrl),
+            editConfigurationFilePut("etc/org.fcrepo.camel.indexing.solr.cfg", "fcrepo.baseUrl", fcrepoBaseUrl),
+            editConfigurationFilePut("etc/org.fcrepo.camel.indexing.solr.cfg", "input.stream", emptyTopic),
+            editConfigurationFilePut("etc/org.fcrepo.camel.reindexing.cfg", "fcrepo.baseUrl", fcrepoBaseUrl),
+            editConfigurationFilePut("etc/org.fcrepo.camel.reindexing.cfg", "rest.port", reindexingPort),
+            editConfigurationFilePut("etc/org.fcrepo.camel.serialization.cfg", "fcrepo.baseUrl", fcrepoBaseUrl),
+            editConfigurationFilePut("etc/org.fcrepo.camel.serialization.cfg", "serialization.descriptions",
+                    "data/tmp/descriptions"),
+            editConfigurationFilePut("etc/org.fcrepo.camel.service.activemq.cfg", "jms.brokerUrl", brokerUrl)
+       };
+    }
+
+    @Test
+    public void testInstallation() throws Exception {
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("camel-core")));
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("fcrepo-camel")));
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("acrepo-idiomatic")));
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("acrepo-idiomatic-pgsql")));
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("acrepo-mint-service")));
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("acrepo-xml-metadata")));
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("acrepo-jsonld-service")));
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("acrepo-jsonld-cache")));
+        assertTrue(featuresService.isInstalled(featuresService.getFeature("acrepo-template-mustache")));
+    }
+}
